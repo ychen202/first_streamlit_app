3b9377886c54e0b4fcebbfae0486a144ffd3fbf5
ychen202 <146498641+ychen202@users.noreply.github.com>
2023-09-30 12:13:29 +1000

    Update streamlit_app.py
    fix the fruits_selected issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # predefined fruits and pull the data from fruits_selected to fruits_to_show
-fruits_select = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
