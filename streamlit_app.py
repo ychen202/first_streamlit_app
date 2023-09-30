dae4fe21200516e87b6728770d8b08e299efa95c
ychen202 <146498641+ychen202@users.noreply.github.com>
2023-09-30 11:59:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg \n  🥑🍞 Avocado Toast')
+streamlit.text('🐔Hard-Boiled Free-Range Egg \n🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
