08a449a61d6f018d905d7ea78a034ac1af84468e
ychen202 <146498641+ychen202@users.noreply.github.com>
2023-09-30 11:52:33 +1000

    Update streamlit_app.py
    fix the 2 lines of text issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega & Blueberry Oatmeal
-Kale, Spinach & Rocket Smoothie')
+streamlit.text('Omega & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
